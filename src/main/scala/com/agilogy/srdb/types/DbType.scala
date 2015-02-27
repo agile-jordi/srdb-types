1a32d84cece4358fd3769dde18ec704c10cf5f5b
Jordi Pradel Miquel <jordi.pradel@agilogy.com>
2015-02-27 09:45:16 +0100

    Remove useless PositionalDbType
    All db types are positional, so merge DbType and PositionalDbType into just one trait

--- src/main/scala/com/agilogy/srdb/types/DbType.scala
+++ src/main/scala/com/agilogy/srdb/types/DbType.scala
@@ -6,13 +6,11 @@ private[types] case object HasLength0{
   val length:Int = 0
 }
 
-sealed trait DbType[T] extends DbReader[T] with DbWriter[T]{
+trait DbType[T] extends PositionalDbReader[T] with DbWriter[T]{
   protected val t0 = HasLength0
 }
 
-trait PositionalDbType[T] extends PositionalDbReader[T] with DbType[T]
-
-case class NotNullAtomicDbType[T](implicit columnType:ColumnType[T]) extends PositionalDbType[T] {
+case class NotNullAtomicDbType[T](implicit columnType:ColumnType[T]) extends DbType[T] {
 
   override val length: Int = 1
 
@@ -22,7 +20,7 @@ case class NotNullAtomicDbType[T](implicit columnType:ColumnType[T]) extends Pos
 
 }
 
-case class OptionalAtomicDbType[T](implicit columnType: ColumnType[T]) extends PositionalDbType[Option[T]]{
+case class OptionalAtomicDbType[T](implicit columnType: ColumnType[T]) extends DbType[Option[T]]{
 
   override val length: Int = 1
 
